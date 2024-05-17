7014516736a8b610c320e6eb4b32b23b0d065ba3
MinJeung-Kim <focso5@gmail.com>
2024-05-17 20:11:56 +0900

    Add: 이미지 추가 기능

--- src/components/Sketch/Sketch.tsx
+++ src/components/Sketch/Sketch.tsx
@@ -17,6 +17,7 @@ const FabricCanvas: React.FC = () => {
   }>({ left: 0, top: 0 });
   const inputRef = useRef<HTMLInputElement>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (canvasRef.current) {
@@ -299,6 +300,33 @@ const FabricCanvas: React.FC = () => {
     }
   };
 
+  const addImageToCanvas = (file: File) => {
+    if (canvas) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        fabric.Image.fromURL(e.target?.result as string, (img) => {
+          img.set({
+            left: canvas.width! / 2,
+            top: canvas.height! / 2,
+            originX: "center",
+            originY: "center",
+          });
+          canvas.add(img);
+          saveState(canvas);
+        });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleImageFileChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.target.files && event.target.files.length > 0) {
+      addImageToCanvas(event.target.files[0]);
+    }
+  };
+
   useEffect(() => {
     if (canvas) {
       canvas.on("mouse:down", handleCanvasClick);
@@ -339,6 +367,14 @@ const FabricCanvas: React.FC = () => {
         ref={fileInputRef}
         style={{ display: "none" }}
         onChange={handleFileChange}
+      />
+      <button onClick={() => imageInputRef.current?.click()}>Set Image</button>
+      <input
+        type="file"
+        accept="image/*"
+        ref={imageInputRef}
+        style={{ display: "none" }}
+        onChange={handleImageFileChange}
       />
       <canvas
         ref={canvasRef}
